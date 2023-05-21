3ff5298d5e584b0fac741bc7e3251d86965ba275
Dylan LANNUZEL <lannuzeld@gmail.com>
2023-05-21 02:14:21 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+
+<template>
+    <div class="bg-gray-800 h-screen text-white">
+        <header class="p-16">
+            <h1 class="text-6xl font-bold text-center">Portfolio</h1>
+        </header>
+        <div>
+            <p class="text-center text-xl">En cours de construction</p>
+        </div>
+    </div>
+</template>
+<script setup>
+</script>

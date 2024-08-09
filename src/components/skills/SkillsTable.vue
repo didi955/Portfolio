a8a35443dd7c6644fd931e316318b16d095f94b0
Dylan LANNUZEL <lannuzeld@gmail.com>
2024-08-10 00:57:18 +0200

    reformat code

--- src/components/skills/SkillsTable.vue
+++ src/components/skills/SkillsTable.vue
@@ -1,38 +1,32 @@
-<script setup lang="ts">
+<script lang="ts" setup>
 
-  import { PropType } from 'vue';
-  import { Skill } from "../../interfaces/types.ts";
+import {Skill} from "../../interfaces/types.ts";
 
-  defineProps({
-    title: {
-      type: String,
-      required: true,
-    },
-    skills: {
-      type: Array as PropType<Skill[]>,
-      required: true,
-    },
-  });
+defineProps<{
+    title: string,
+    skills: Skill[]
+}>();
 
 </script>
 
 <template>
 
-  <div class="bg-gray-200 rounded-lg shadow-lg p-4 transition-all duration-200 hover:border-cyan-500 hover:border hover:outline hover:outline-3 hover:outline-cyan-400/20">
-    <h3 class="text-xl font-semibold mb-2 text-gray-600">{{ title }}</h3>
+    <div
+        class="bg-gray-200 rounded-lg shadow-lg p-4 transition-all duration-200 hover:border-cyan-500 hover:border hover:outline hover:outline-3 hover:outline-cyan-400/20">
+        <h3 class="text-xl font-semibold mb-2 text-gray-600">{{ title }}</h3>
 
-    <div class="flex flex-wrap">
+        <div class="flex flex-wrap">
 
-      <div v-for="(skill, index) in skills" :key="index" class="w-1/2 p-2">
-        <div class="flex items-center">
-          <a :href="skill.link" target="_blank">
-            <img :src="skill.logo" :alt="skill.name + ' icon'" class="h-8 w-8 mr-2" loading="lazy">
-            <span>{{ skill.name }}</span>
-          </a>
+            <div v-for="(skill, index) in skills" :key="index" class="w-1/2 p-2">
+                <div class="flex items-center">
+                    <a :href="skill.link" target="_blank">
+                        <img :alt="skill.name + ' icon'" :src="skill.logo" class="h-8 w-8 mr-2" loading="lazy">
+                        <span>{{ skill.name }}</span>
+                    </a>
+                </div>
+            </div>
+
         </div>
-      </div>
-
     </div>
-  </div>
 
 </template>
